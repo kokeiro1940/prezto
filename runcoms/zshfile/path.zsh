18eec08c2d601df7c16a6998f99dd540a21e1a4e
tonomura <tonomura@tech-d137.bp.mfeed.ad.jp>
2019-03-31 03:29:06 +0900

    add and edit files

---
+++ runcoms/zshfile/path.zsh
@@ -0,0 +1,15 @@
+# nginx
+export PATH="/usr/local/sbin:$PATH"
+
+# node
+export PATH="$HOME/.nodebrew/current/bin:$PATH"
+export PATH="$HOME/.yarn/bin:$PATH"
+
+# pyenv
+eval "$(pyenv init -)"
+export PATH="$HOME/.pyenv/shims:$PATH"
+
+# rbenv
+export PATH="$HOME/.rbenv/shims:$PATH"
+eval "$(rbenv init -)"
+
