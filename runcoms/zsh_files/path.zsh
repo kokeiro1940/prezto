ce044f63ef46005d9f210c183885b3c67d051e09
tonomura <tonomura@MFPC-1005.local>
2019-04-01 22:30:37 +0900

    add -many-files

--- runcoms/zsh_files/path.zsh
+++ runcoms/zsh_files/path.zsh
@@ -3,6 +3,8 @@ export PATH="/usr/local/sbin:$PATH"
 
 # node
 export PATH="$HOME/.nodebrew/current/bin:$PATH"
+
+# yarn
 export PATH="$HOME/.yarn/bin:$PATH"
 
 # pyenv
@@ -10,7 +12,7 @@ if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 export PATH="$HOME/.pyenv/bin:$PATH"
 # eval "$(pyenv init -)"
 
-# pyenv 以前うまくいっていたもの
+# pyenv before
 # export PATH="$HOME/.pyenv/shims:$PATH"
 # eval "$(pyenv init -)"
 
@@ -19,7 +21,7 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 export PATH="$HOME/.rbenv/bin:$PATH"
 # eval "$(rbenv init -)"
 
-# rbenv 以前うまくいっていたもの
+# rbenv before
 # export PATH="$HOME/.rbenv/shims:$PATH"
 # eval "$(rbenv init -)"
 
