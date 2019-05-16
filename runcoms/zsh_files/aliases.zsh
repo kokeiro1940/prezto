706cd049639f4f60d7c082e68ff8f56974e66f6d
Atsuki-Tonomura <tonomura@mfeed.ad.jp>
2019-05-16 16:22:41 +0900

    add one_liner

--- runcoms/zsh_files/aliases.zsh
+++ runcoms/zsh_files/aliases.zsh
@@ -1,7 +1,7 @@
 # git
-alias gstas='git status'
-alias gdif='git diff'
-alias glog='git log'
+alias gstas='git status '
+alias gdif='git diff '
+alias glog='git log '
 alias gshow='git show '
 alias gadd='git add '
 alias gcomm='git commit -m '
@@ -10,4 +10,9 @@ alias gcheo='git checkout '
 alias gpull='git pull '
 
 # one_liner
-alias lf='find ./ -type f -print | xargs grep '
+# looking for the files which include the word ARG
+alias lfw='find ./ -type f -print | xargs grep '
+# looking for the files which TITLE include the word ARG
+alias lfn='find ./ -name '
+# delete .DS_Store
+alias rm_dsstore='sudo find ./ -name ".DS_Store" -delete'
