d2bca99c89097979b1a4550536e6a41c148537fc
kokeiro1940 <kokeiro1940@gmail.com>
2019-11-02 22:45:44 +0900

    edit alias

--- runcoms/zsh_files/aliases.zsh
+++ runcoms/zsh_files/aliases.zsh
@@ -1,10 +1,12 @@
-# one_liner
+# one_line
 # looking for the files which include the word ARG
 alias lfw='find ./ -type f -print | xargs grep '
 # looking for the files which TITLE include the word ARG
 alias lfn='find ./ -name '
 # delete .DS_Store
 alias rm_dsstore='sudo find ./ -name ".DS_Store" -delete'
-
 # google chrome
 alias chrome='open /Applications/Google\ Chrome.app '
+
+# TODO: exec JavaScript on mac
+alias js='osascript -l JavaScript '
